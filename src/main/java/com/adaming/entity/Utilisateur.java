409bea9a4e39f904f64df21f50c8928eaa229575
Nico <fillion.nicolas33@gmail.com>
2020-11-16 11:28:21 +0100

    1st push

---
+++ src/main/java/com/adaming/entity/Utilisateur.java
@@ -0,0 +1,79 @@
+package com.adaming.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Utilisateur implements Serializable{
+
+	//Attributes
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUtilisateur;
+	private String nomUtilisateur;
+	private String prenomUtilisateur;
+	@Column(unique=true)
+	private String username;
+	private String password;
+
+	//Constructors
+	public Utilisateur() {
+	}
+	public Utilisateur(String nomUtilisateur, String prenomUtilisateur, String username,
+			String password) {
+		this.nomUtilisateur = nomUtilisateur;
+		this.prenomUtilisateur = prenomUtilisateur;
+		this.username = username;
+		this.password = password;
+	}
+
+	//Getters Setters
+	public Long getIdUtilisateur() {
+		return idUtilisateur;
+	}
+	public void setIdUtilisateur(Long idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+	public String getNomUtilisateur() {
+		return nomUtilisateur;
+	}
+	public void setNomUtilisateur(String nomUtilisateur) {
+		this.nomUtilisateur = nomUtilisateur;
+	}
+	public String getPrenomUtilisateur() {
+		return prenomUtilisateur;
+	}
+	public void setPrenomUtilisateur(String prenomUtilisateur) {
+		this.prenomUtilisateur = prenomUtilisateur;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "Utilisateur [idUtilisateur=" + idUtilisateur + ", nomUtilisateur=" + nomUtilisateur
+				+ ", prenomUtilisateur=" + prenomUtilisateur + ", username=" + username + ", password=" + password
+				+ "]";
+	}
+	
+}
---
+++ src/main/java/com/adaming/service/UtilisateurService.java
@@ -0,0 +1,19 @@
+package com.adaming.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.adaming.entity.Utilisateur;
+
+public interface UtilisateurService {
+	//Save & Update
+	public Utilisateur souUtilisateur(Utilisateur utilisateur);
+	//Delete
+	public void deleteUtilisateurById(Long id);
+	//Find One By Id
+	public Optional<Utilisateur> findUtilisateurById(Long id);
+	//Get One
+	public Utilisateur findUtilisateur(Long id);
+	//Find All
+	public List<Utilisateur> findUtilisateurs();
+}
